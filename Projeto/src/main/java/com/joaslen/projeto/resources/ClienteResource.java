80a4c70d7779a45a76f9879f2967e1d628437197
Joaslen-Ribeiro <joaslen@hotmail.com>
2021-10-04 09:19:45 -0300

    inserindo post

--- Projeto/src/main/java/com/joaslen/projeto/resources/ClienteResource.java
+++ Projeto/src/main/java/com/joaslen/projeto/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.joaslen.projeto.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import com.joaslen.projeto.domain.Cliente;
 import com.joaslen.projeto.dto.ClienteDTO;
+import com.joaslen.projeto.dto.ClienteNewDTO;
 import com.joaslen.projeto.services.ClienteService;
 
 @RestController
@@ -32,6 +36,15 @@ public class ClienteResource {
 			
 			return ResponseEntity.ok().body(obj);
 			
+		}
+		
+		@RequestMapping(method=RequestMethod.POST)
+		public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+			Cliente obj = service.fromDTO(objDto);
+			obj = service.insert(obj);
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
 		}
 	
 		@RequestMapping(value="/{id}", method=RequestMethod.PUT)
--- Projeto/src/main/java/com/joaslen/projeto/services/ClienteService.java
+++ Projeto/src/main/java/com/joaslen/projeto/services/ClienteService.java
@@ -10,9 +10,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+
+import com.joaslen.projeto.domain.Cidade;
 import com.joaslen.projeto.domain.Cliente;
+import com.joaslen.projeto.domain.Endereco;
+import com.joaslen.projeto.domain.enums.TipoCliente;
 import com.joaslen.projeto.dto.ClienteDTO;
+import com.joaslen.projeto.dto.ClienteNewDTO;
 import com.joaslen.projeto.repositories.ClienteRepository;
+import com.joaslen.projeto.repositories.EnderecoRepository;
 import com.joaslen.projeto.services.services.exception.DataIntegrityException;
 import com.joaslen.projeto.services.services.exception.ObjectNotFoundException;
 
@@ -22,6 +28,11 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
+	
+	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
 
@@ -29,6 +40,14 @@ public class ClienteService {
 		return obj.orElseThrow(()-> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+ id + ", Tipo: "+ Cliente.class.getName()));
 }
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
+	}
+	
 	
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -56,6 +75,22 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade (objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli,cid );
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2()!= null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3()!= null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		return cli;
+		
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
