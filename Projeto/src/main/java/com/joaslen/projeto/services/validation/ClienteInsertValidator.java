829059d8ae1f8435b2daa3482824f5b56b510dc5
Joaslen-Ribeiro <joaslen@hotmail.com>
2021-10-06 09:08:56 -0300

    validacao de email

--- Projeto/src/main/java/com/joaslen/projeto/services/validation/ClienteInsertValidator.java
+++ Projeto/src/main/java/com/joaslen/projeto/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.joaslen.projeto.domain.Cliente;
 import com.joaslen.projeto.domain.enums.TipoCliente;
 import com.joaslen.projeto.dto.ClienteNewDTO;
+import com.joaslen.projeto.repositories.ClienteRepository;
 import com.joaslen.projeto.resources.Exception.FieldMessage;
 import com.joaslen.projeto.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
