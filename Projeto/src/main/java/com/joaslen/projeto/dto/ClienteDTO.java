cd28bc56b920e5635ae57a1270632b95d60c3fcc
Joaslen-Ribeiro <joaslen@hotmail.com>
2021-10-06 09:34:24 -0300

    validacao cliente

--- Projeto/src/main/java/com/joaslen/projeto/dto/ClienteDTO.java
+++ Projeto/src/main/java/com/joaslen/projeto/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.joaslen.projeto.domain.Cliente;
+import com.joaslen.projeto.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
 	private static final long serialVersionUID = 1L;   
 	
